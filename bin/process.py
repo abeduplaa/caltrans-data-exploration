88d9ac18a594ae973522021aa52c18ac8eb049ca
abeduplaa <abrahamduplaa@gmail.com>
2019-04-01 00:57:49 +0200

    added lines to send pre-joined table

--- bin/process.py
+++ bin/process.py
@@ -35,13 +35,24 @@ def using_reset_index(df):
     return df.groupby('STATION').resample('60min').max()
 
 
+def add_rounded_hour_column(df, name_current_ts='timestamp_', name_rounded_ts='timestamp_rounded', round_by='H'):
+    df[name_rounded_ts] = df[name_current_ts].dt.round(round_by)
+    return df
+
 
 if __name__ == "__main__":
-    send_weather = True
-    send_traffic = False
 
-    traffic_table_name = "traffic_test4_encoded_strkey"
-    weather_table_name = "ncdc_weather_janfeb_dictstrkey"
+    send_weather = False
+    send_traffic = True
+    send_joined = True
+    send_meta_traffic = False
+    send_meta_weather = False
+
+    traffic_table_name = "weather_traffic_janfeb_joined"
+    weather_table_name = "ncdc_weather_janfeb_dictstrkey_test"
+
+    traffic_meta_table_name = "caltrans_traffic_janfeb_d07_metatable"
+    weather_meta_table_name = "ncdc_weather_janfeb_sanfrancisco_metatable"
 
     if len(sys.argv) != 2:
         print(len(sys.argv))
@@ -51,7 +62,7 @@ if __name__ == "__main__":
     config_path = sys.argv[1]
     config = ConfigParser()
     config.read(config_path)
-    print("Config file read.")
+    print("Configuration file read.")
 
     # traffic information for loading in data
     traffic_data_columns = ['timestamp_',
@@ -189,7 +200,6 @@ if __name__ == "__main__":
     weather_data[weather_station_id] = weather_data['STATION'].str[-5:]
     weather_data = weather_data.drop(columns='STATION')
     weather_data = weather_data.reset_index(level=0)
-    #weather_data[weather_station_id] = weather_data['STATION'].str[-5:]
     weather_data = weather_data.loc[weather_startdate:weather_enddate]
     weather_data = raw_weather_data.fillna(0)
     weather_data = weather_data.reset_index()
@@ -201,19 +211,45 @@ if __name__ == "__main__":
     # get the paths of relevant files for the data
     csv_files = config.get('Paths', 'data_path')
 
-    # # Send data to omnisci:
+    # Send data to omnisci:
     print("connect to omnisci")
     connection = OmnisciConnect(config_path)
     connection.start_connection()
 
+    # send traffic metadata:
+    if send_meta_traffic:
+        print("sending %s to omnisci" % traffic_meta_table_name)
+        connection.load_data(table_name=traffic_meta_table_name, df=df_traffic_metadata, method='infer', create='infer')
+
+    # send weather metadata:
+    if send_meta_weather:
+        print("sending %s to omnisci" % weather_meta_table_name)
+        connection.load_data(table_name=weather_meta_table_name, df=df_weather_metadata, method='infer', create='infer')
+
     # send weather data
     if send_weather:
         print("sending weather data to omnisci")
-        weather_data[weather_station_id] = weather_data['station'].str[-5:]
+        #weather_data[weather_station_id] = weather_data['station'].str[-5:]
         #weather_data[weather_station_id] = pd.to_numeric(weather_data[weather_station_id])
+
+        weather_data = utils.downcast_type(weather_data)
         print(weather_data.dtypes)
         connection.load_data(table_name=weather_table_name, df=weather_data, method='infer', create='infer')
 
+    if send_joined:
+        weather_data[weather_station_id] = weather_data['station'].str[-5:]
+        #weather_data = weather_data.rename(index=str, columns={"station": weather_station_id})
+        #weather_data = utils.downcast_type(weather_data)
+        print(weather_data.dtypes)
+
+        ts_rounded = 'timestamp_rounded'
+        print(weather_data[weather_station_id].head())
+        join_key = [weather_station_id, ts_rounded]
+        weather_data = add_rounded_hour_column(weather_data, name_rounded_ts=ts_rounded)
+        grouped_weather_data = weather_data.groupby(join_key).mean()
+        grouped_weather_data = grouped_weather_data.reset_index()
+
+    # send traffic metadata:
     if send_traffic:
         # get file paths:
         file_paths = utils.get_file_names(csv_files, extension=file_ext)
@@ -233,23 +269,28 @@ if __name__ == "__main__":
             df_extracted_traffic = df_extracted_traffic.drop('district', axis=1)
 
             df_extracted_traffic = df_extracted_traffic.join(df_traffic_metadata, on='station')
-            # df_extracted_traffic[weather_station_id] = df_extracted_traffic[weather_station_id].astype(int)
 
             print("applying transformations to traffic data")
             df_transformed_traffic = apply_custom_transformations(df=df_extracted_traffic,
-                                                              interest_col=interest_col,
-                                                              threshold=threshold,
-                                                              grouper=grouper)
+                                                                  interest_col=interest_col,
+                                                                  threshold=threshold,
+                                                                  grouper=grouper)
 
-            #print(df_transformed_traffic.isna().sum())
             df_transformed_traffic[weather_station_id] = df_transformed_traffic[weather_station_id].astype(int)
             df_transformed_traffic[weather_station_id] = df_transformed_traffic[weather_station_id].astype(str)
-            print(df_transformed_traffic.dtypes)
-            print(df_transformed_traffic.isna().sum())
-            connection.load_data(table_name=traffic_table_name, df=df_transformed_traffic, method='infer', create='infer')
+
+            if send_joined:
+                df_transformed_traffic = add_rounded_hour_column(df_transformed_traffic, name_rounded_ts=ts_rounded)
+                df_transformed_traffic[weather_station_id] = df_transformed_traffic[weather_station_id].astype(str)
+                df_transformed_traffic = pd.merge(left=df_transformed_traffic,
+                                                  right=grouped_weather_data,
+                                                  how='inner',
+                                                  on=join_key)
+
+            connection.load_data(table_name=traffic_table_name,
+                                 df=df_transformed_traffic,
+                                 method='infer',
+                                 create='infer')
 
     connection.close_connection()
 
-    # df_transformed_traffic = df_transformed_traffic.join(weather_data, on=weather_station_id)
-    # df_traffic_weather = pd.merge(df_transformed_traffic,weather_data,on=weather_station_id)
-    # weather_data[weather_station_id] = weather_data['STATION'].str[-5:]
