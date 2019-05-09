7e50165fa5a9f01d949106c9213223a34b4b787a
abeduplaa <abrahamduplaa@gmail.com>
2019-05-10 00:15:42 +0200

    made multi-threading possible with pymapd

--- process_traffic_data.py
+++ process_traffic_data.py
@@ -1,9 +1,12 @@
 from configparser import ConfigParser
 import sys
 import pandas as pd
+import threading
+import time
 
 import data_processing.process_utils as utils
 from omnisci_connector.omni_connect import OmnisciConnect
+
 
 
 def apply_custom_transformations(df, interest_col, threshold, grouper):
@@ -12,10 +15,10 @@ def apply_custom_transformations(df, interest_col, threshold, grouper):
     df = utils.grouped_drop_na(df, threshold, grouper=grouper, col=interest_col)
 
     # lower all column names
+
     df = utils.lower_col_names(df)
 
     df['state_pm'] = utils.state_pm_to_numeric(df['state_pm'])
-
     # drop all rows with na in absolute postmarker field
     df = df.dropna(subset=['abs_pm'])
 
@@ -33,7 +36,7 @@ def apply_custom_transformations(df, interest_col, threshold, grouper):
     df['day_of_year'] = df['timestamp_'].dt.dayofyear
 
     # downcast all ints to save memory
-    df = utils.downcast_int(df, ['station', 'freeway', 'samples', 'total_flow', 'lanes', 'county'])
+    df = utils.downcast_int(df, ['station', 'freeway', 'total_flow', 'lanes', 'county'])
 
     # downcast all floats to save memory
     df = utils.downcast_type(df)
@@ -41,41 +44,115 @@ def apply_custom_transformations(df, interest_col, threshold, grouper):
     return df
 
 
-send_traffic = True
-table_name = "historic_test"
+table_name = "caltrans_historic_2015_2019"
 
 # initial parameters for reading in traffic data
 threshold = 0.1
 interest_col = 'speed'
 grouper = 'station'
-batch_limit = 1
+batch_limit = 3
 file_ext = '.txt'
+thread_num = 3
+DEBUG = True
 
 
-# traffic information for loading in data
-traffic_data_columns = ['timestamp_',
-                        'station',
-                        'district',
-                        'freeway',
-                        'direction',
-                        'lane_type',
-                        'station_length',
-                        'samples',
-                        'pct_observed',
-                        'total_flow',
-                        'occupancy',
-                        'speed'
-                        ]
+class TrafficData:
+    data_columns = ['timestamp_',
+                    'station',
+                    'district',
+                    'freeway',
+                    'direction',
+                    'lane_type',
+                    'station_length',
+                    'samples',
+                    'pct_observed',
+                    'total_flow',
+                    'occupancy',
+                    'speed'
+                    ]
 
-traffic_meta_columns = ['ID',
-                        'County',
-                        'State_PM',
-                        'Abs_PM',
-                        'Latitude',
-                        'Longitude',
-                        'Lanes',
-                        'Name']
+    meta_columns = ['id',
+                    'county',
+                    'state_pm',
+                    'abs_pm',
+                    'latitude',
+                    'longitude',
+                    'lanes']
 
+    def __init__(self, files, meta_file):
+        self.files = files
+        self.meta_file = meta_file
+        self.metadata = None
+        self.data = None
+        self.df = None
+
+        # Read in traffic data
+        self.read_data()
+
+        # Read in metatable
+        self.read_meta()
+
+        # Join metatable and data
+        self.create_df()
+
+    def read_meta(self):
+        self.metadata = (pd.read_csv(self.meta_file, sep=',', usecols=self.meta_columns)
+                         .rename(str.lower, axis='columns')
+                         .set_index('id'))
+
+    def read_data(self):
+        no_data_cols = list(range(len(self.data_columns)))
+        l_df = []
+        for f in self.files:
+            print("Processing file: ", f)
+            temp = (pd.read_csv(f, header=None, names=self.data_columns, usecols=no_data_cols)
+                    .drop('district', axis=1)
+                    .drop('samples', axis=1)
+                    .drop('pct_observed',axis=1))
+            l_df.append(temp)
+
+        self.data = pd.concat(l_df, ignore_index=True)
+
+    def create_df(self):
+        self.df = self.data.join(self.metadata, on='station')
+
+
+def transform_and_load(config_path, paths, meta_path, thread):
+
+    # Send data to omnisci:
+    print("connect to omnisci with thread: ", thread)
+    connection = OmnisciConnect(config_path)
+    connection.start_connection()
+
+    df_batch = []
+
+    try:
+        data = TrafficData(paths, meta_path)
+
+
+        df_transformed = apply_custom_transformations(df=data.df,
+                                                          interest_col=interest_col,
+                                                          threshold=threshold,
+                                                          grouper=grouper)
+    except ValueError as ex:
+        print(ex)
+        print("could not process ", paths[0])
+        return
+
+    connection.load_data(table_name=table_name,
+                         df=df_transformed,
+                         method='infer',
+                         create='infer')
+
+    connection.close_connection()
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+t0 = time.time()
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
@@ -88,55 +165,38 @@ if __name__ == "__main__":
     config.read(config_path)
     print("Configuration file read.")
 
-    ### metadata section ###
     traffic_meta_path = config.get('Paths', 'meta_path')
-
-    # read in the traffic metadata to pandas:
-    df_traffic_metadata = pd.read_csv(traffic_meta_path, sep='\t', usecols=traffic_meta_columns).set_index('ID')
-    df_traffic_metadata = df_traffic_metadata.rename(str.lower, axis='columns')
-    print("traffic metadata file read.")
-
-    ### Traffic Section ###
-
-    # get the paths of relevant files for the data
     csv_files = config.get('Paths', 'data_path')
-
-    # Send data to omnisci:
-    print("connect to omnisci")
-    connection = OmnisciConnect(config_path)
-    connection.start_connection()
+    print(csv_files)
+    file_paths = utils.get_file_names(csv_files, extension=file_ext)
 
 
-    # send traffic metadata:
-    if send_traffic:
-        # get file paths:
-        file_paths = utils.get_file_names(csv_files, extension=file_ext)
-        print("Number of traffic files found: ", len(file_paths))
 
-        # extract and traffic data in batches:
-        no_data_cols = list(range(len(traffic_data_columns)))
-        for i in range(0, len(file_paths), batch_limit):
-        #for i in range(0, batch_limit, batch_limit):
-            df_batch = []
-            for f in file_paths[i:i + batch_limit]:
-                print("Processing file: ", f)
-                temp = pd.read_csv(f, header=None, names=traffic_data_columns, usecols=no_data_cols)
-                df_batch.append(temp)
 
-            df_extracted_traffic = pd.concat(df_batch, ignore_index=True)
+    print("Number of traffic files found: ", len(file_paths))
 
-            df_extracted_traffic = df_extracted_traffic.drop('district', axis=1)
+    if DEBUG:
+        end = batch_limit
+    else:
+        end = len(file_paths)
 
-            df_extracted_traffic = df_extracted_traffic.join(df_traffic_metadata, on='station')
+    for i in range(0, end, batch_limit):
+        files = file_paths[i:i + batch_limit]
 
-            df_transformed_traffic = apply_custom_transformations(df=df_extracted_traffic,
-                                                                  interest_col=interest_col,
-                                                                  threshold=threshold,
-                                                                  grouper=grouper)
+        #transform_and_load(end, batch_limit, file_paths, traffic_meta_path)
 
-            connection.load_data(table_name=table_name,
-                                 df=df_transformed_traffic,
-                                 method='infer',
-                                 create='infer')
+        threads = []
+        for j in range(thread_num):
+            n = batch_limit / thread_num
+            assert batch_limit % thread_num == 0
+            chunked_files = list(chunks(files, int(n)))
+            t = threading.Thread(target=transform_and_load, args=(config_path, chunked_files[j], traffic_meta_path,j,))
+            threads.append(t)
+            t.start()
 
-    connection.close_connection()
+    for t in threads:
+        t.join()
+
+print("Exiting Main Thread")
+
+print("total time: " , time.time()-t0)
