ec80a39938fd59b9825f27ae3aa21f4908c9c0e8
abeduplaa <abrahamduplaa@gmail.com>
2019-05-23 07:06:37 +0000

    add new functions to training_utils.py

--- training/train_utils.py
+++ training/train_utils.py
@@ -1,8 +1,14 @@
+from tensorflow.python.keras.models import load_model
+
+
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_squared_error
 import pandas as pd
 import numpy as np
+import math
+import statsmodels.api as sm
+from sklearn.metrics import mean_squared_error
+
 
 def data_index(df, n_lag, n_steps):
     
@@ -76,4 +82,56 @@ def prepare_data_for_network(data, n_in=1, n_out=1, dropnan=True):
         agg.dropna(inplace=True)
     return agg
 
+def remove_cols(df):
+    
+    cols = list(df.columns)
+    drop_1 = [c for c in cols if '(t+' in c]
+    drop_2 = [c for c in cols if '(t)' in c]
 
+    drop_1.remove('var2(t+5)')
+
+    drop_cols = drop_1 + drop_2
+    df = df.drop(drop_cols, axis=1)
+    
+    return df
+
+
+def predict_data(df, model, scaler1):
+    
+    test_X, test_y = prepare_data(df)
+    
+    my_model = load_model(model)
+    
+    # make a prediction
+    yhat = my_model.predict(test_X)
+    print('yhat predicted')
+    test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+    
+    # invert scaling for forecast
+    inv_yhat = np.concatenate((yhat, test_X[:, 1:]), axis=1)
+    inv_yhat = scaler1.inverse_transform(inv_yhat)
+    inv_yhat = inv_yhat[:,0]
+
+    # invert scaling for actual
+    test_y = test_y.reshape((len(test_y), 1))
+    inv_y = np.concatenate((test_y, test_X[:, 1:]), axis=1)
+    inv_y = scaler1.inverse_transform(inv_y)
+    inv_y = inv_y[:,0]
+
+    # calculate RMSE
+    rmse = math.sqrt(mean_squared_error(inv_y, inv_yhat))
+    print('Test RMSE: %.3f' % rmse)
+    
+    return inv_y, inv_yhat, rmse
+
+def prepare_data(df):
+    test = df.values
+    
+    # split into input and outputs
+    test_X, test_y = test[:, :-1], test[:, -1]
+    
+    test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+    
+    print(test_X.shape, test_y.shape)
+    
+    return test_X, test_y
