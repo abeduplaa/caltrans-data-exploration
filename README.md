e14eca2850c748579682c11865a22748d931cb73
abeduplaa <abrahamduplaa@gmail.com>
2019-05-23 23:34:32 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,25 +1,11 @@
-OmniSciData exploration with Caltrans Data
+OmniSci Data exploration of Caltrans Data
 ==============================================
 
 Use python 3+ and install the requirements.
 
 At the moment, the following steps are needed to download the data:
-1. copy the html from the caltrans website for the year wanted and place in the html directory
+1. copy the links from the caltrans website for the year wanted and place in the html directory (if extraction is necessary)
 2. make sure the directories and login info in the config file is correct
-
-## to extract data from caltrans
-you'll need to first make sure everything is correct in the config.ini file.
-
-Then run the command:
-```
-python /bin/extract.py /path/to/your/config.ini/file
-```
-
-## after extraction process the data by running
-```
-python /bin/process.py /path/to/your/config.ini/file
-```
-In process.py, there are some parameters that can be adjusted
 
 ## How to use:
 
@@ -28,7 +14,6 @@ The best way to start is to go through the jupyter notebooks in this order (all 
 1. Extracting Data from Caltrans: Extract_Data.ipynb
 2. Playing/Transforming data from Caltrans and pulling weather data: Processing_Traffic_Weather.ipynb
 3. Try out some of the training and testing notebooks and/or make your own models!
-
 
 ___
 
--- process_traffic_data.py
+++ process_traffic_data.py
@@ -6,115 +6,19 @@ import time
 
 import data_processing.process_utils as utils
 from omnisci_connector.omni_connect import OmnisciConnect
-
-
-
-def apply_custom_transformations(df, interest_col, threshold, grouper):
-
-    # drop nas
-    df = utils.grouped_drop_na(df, threshold, grouper=grouper, col=interest_col)
-
-    # lower all column names
-
-    df = utils.lower_col_names(df)
-
-    df['state_pm'] = utils.state_pm_to_numeric(df['state_pm'])
-    # drop all rows with na in absolute postmarker field
-    df = df.dropna(subset=['abs_pm'])
-
-    df['timestamp_'] = pd.to_datetime(df['timestamp_'], infer_datetime_format=True)
-
-    # add a rounded timestamp for grouping later on: (e.g. 12:46 --> 13:00)
-    df = utils.rounded_timestamp(df=df, name_current_ts='timestamp_', name_rounded_ts='timestamp_rounded', round_by='H')
-
-    df = utils.add_day_of_week(df, 'timestamp_')
-
-    # add column with hour of day for each data point
-    df['hour_of_day'] = df['timestamp_'].dt.hour
-
-    # add column with day of year for each data point
-    df['day_of_year'] = df['timestamp_'].dt.dayofyear
-
-    # downcast all ints to save memory
-    df = utils.downcast_int(df, ['station', 'freeway', 'total_flow', 'lanes', 'county'])
-
-    # downcast all floats to save memory
-    df = utils.downcast_type(df)
-
-    return df
-
+from data_processing.MetaData import MetaData
+from data_processing.TrafficData import TrafficData
 
 table_name = "test_2"
 
 # initial parameters for reading in traffic data
-threshold = 0.1
+threshold = 0.01
 interest_col = 'speed'
 grouper = 'station'
 batch_limit = 1
 file_ext = '.txt'
 thread_num = 1
-DEBUG = True
-
-
-class TrafficData:
-    data_columns = ['timestamp_',
-                    'station',
-                    'district',
-                    'freeway',
-                    'direction',
-                    'lane_type',
-                    'station_length',
-                    'samples',
-                    'pct_observed',
-                    'total_flow',
-                    'occupancy',
-                    'speed'
-                    ]
-
-    meta_columns = ['id',
-                    'county',
-                    'state_pm',
-                    'abs_pm',
-                    'latitude',
-                    'longitude',
-                    'lanes']
-
-    def __init__(self, files, meta_file):
-        self.files = files
-        self.meta_file = meta_file
-        self.metadata = None
-        self.data = None
-        self.df = None
-
-        # Read in traffic data
-        self.read_data()
-
-        # Read in metatable
-        self.read_meta()
-
-        # Join metatable and data
-        self.create_df()
-
-    def read_meta(self):
-        self.metadata = (pd.read_csv(self.meta_file, sep=',',usecols=self.meta_columns)
-                         .rename(str.lower, axis='columns')
-                         .set_index('id'))
-
-    def read_data(self):
-        no_data_cols = list(range(len(self.data_columns)))
-        l_df = []
-        for f in self.files:
-            print("Processing file: ", f)
-            temp = (pd.read_csv(f, header=None, names=self.data_columns, usecols=no_data_cols)
-                    .drop('district', axis=1)
-                    .drop('samples', axis=1)
-                    .drop('pct_observed',axis=1))
-            l_df.append(temp)
-
-        self.data = pd.concat(l_df, ignore_index=True)
-
-    def create_df(self):
-        self.df = self.data.join(self.metadata, on='station')
+DEBUG = False
 
 
 def transform_and_load(config_path, paths, meta_path, thread):
@@ -127,10 +31,13 @@ def transform_and_load(config_path, paths, meta_path, thread):
     df_batch = []
 
     try:
-        data = TrafficData(paths, meta_path)
+        traffic_data = TrafficData(paths)
 
+        meta_data = MetaData(meta_path)
 
-        df_transformed = apply_custom_transformations(df=data.df,
+        data = traffic_data.join_meta(meta_df=meta_data.df)
+
+        df_transformed = utils.apply_custom_transformations(df=data,
                                                           interest_col=interest_col,
                                                           threshold=threshold,
                                                           grouper=grouper)
@@ -152,7 +59,7 @@ def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-t0 = time.time()
+
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
@@ -170,9 +77,6 @@ if __name__ == "__main__":
     print(csv_files)
     file_paths = utils.get_file_names(csv_files, extension=file_ext)
 
-
-
-
     print("Number of traffic files found: ", len(file_paths))
 
     if DEBUG:
@@ -182,8 +86,6 @@ if __name__ == "__main__":
 
     for i in range(0, end, batch_limit):
         files = file_paths[i:i + batch_limit]
-
-        #transform_and_load(end, batch_limit, file_paths, traffic_meta_path)
 
         threads = []
         for j in range(thread_num):
@@ -198,5 +100,3 @@ if __name__ == "__main__":
             t.join()
 
 print("Exiting Main Thread")
-
-print("total time: " , time.time()-t0)
