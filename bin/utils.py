78722d84d964c39cbe2dddd886bacf75c37e716f
abeduplaa <abrahamduplaa@gmail.com>
2019-12-03 14:22:46 +0100

    Fixed darksky weather
    now it works

---
+++ bin/utils.py
@@ -0,0 +1,22 @@
+import os
+
+def _check_file(file, error=None):
+    if error is None:
+        error = "Error, file not found"
+    
+    if os.path.isfile(file) is False:
+        raise OSError(error)
+
+
+def locate_config(args):      
+    if len(args) > 1:
+        path = args[1]
+        print(path)
+        error = "user provided config.ini file path not found (e.g., python /path/to/config.ini)"
+    else:
+        path = os.path.join(os.getcwd(),'config.ini')
+        error = "config.ini not found in project directory and not provided by user"    
+        
+    _check_file(path, error)
+    
+    return path
